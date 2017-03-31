db5ab78c723869978f0bc99a0311dc15d3c67ad0
Scott Ewing <Scew5145@colorado.edu>
2017-03-31 16:59:28 -0600

    Song stuff!
    Songs!

--- Assets/Scripts/HatController.cs
+++ Assets/Scripts/HatController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class HatController : MonoBehaviour {
 
 	public float speed;
 	public float jumpForce;
 	public GameObject fireball;
+    public AudioMixerSnapshot fullVolume1;
 	private Rigidbody2D rb;
+    private int comboCount = 0;
 
 	bool grounded = false;
 	public Transform groundCheck;
@@ -18,17 +21,29 @@ public class HatController : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("TriggerLand"))
+        {
+            fullVolume1.TransitionTo(1);
+        }
+    }
 
-	// Update is called once per frame
-	void FixedUpdate () {
+    // Update is called once per frame
+    void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		if (Input.GetAxis("Fire1") == 1)
 		{
 			GameObject newBall = Instantiate(fireball, transform.position, transform.rotation);
 			float randomizer = Random.value*1000;
+            comboCount += 1;
 			newBall.GetComponent<Rigidbody2D>().AddForce(new Vector2(500, randomizer));
 			Destroy(newBall, 1);
 		}
+        if (comboCount >= 100)
+        {
+
+        }
 
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 		if (grounded && Input.GetAxis ("Jump") == 1)
