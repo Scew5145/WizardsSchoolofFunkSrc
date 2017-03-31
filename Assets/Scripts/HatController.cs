e7e152363ede4a149df1d2bda80a30310d465e14
Dean Moser <demo9848@colorado.edu>
2017-03-31 15:23:06 -0600

    jumping behavior and update to general player collision
    Horizontally colliding with ground objects currently results in sticking
    to the wall while the movement key is still pressed. Should address.

--- Assets/Scripts/HatController.cs
+++ Assets/Scripts/HatController.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class HatController : MonoBehaviour {
 
-    public float speed;
-    public GameObject fireball;
-    private Rigidbody2D rb;
+	public float speed;
+	public float jumpForce;
+	public GameObject fireball;
+	private Rigidbody2D rb;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody2D>();
+		rb = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        if (Input.GetAxis("Jump") == 1)
-        {
-            GameObject newBall = Instantiate(fireball, transform.position, transform.rotation);
-            float randomizer = Random.value*1000;
-            newBall.GetComponent<Rigidbody2D>().AddForce(new Vector2(500, randomizer));
-            Destroy(newBall, 1);
-        }
-        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		if (Input.GetAxis("Fire1") == 1)
+		{
+			GameObject newBall = Instantiate(fireball, transform.position, transform.rotation);
+			float randomizer = Random.value*1000;
+			newBall.GetComponent<Rigidbody2D>().AddForce(new Vector2(500, randomizer));
+			Destroy(newBall, 1);
+		}
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		if (grounded && Input.GetAxis ("Jump") == 1)
+		{
+			rb.AddForce (new Vector2(0, jumpForce));
+		}
+		rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
 	}
 }
