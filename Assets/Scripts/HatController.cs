22d889c3b2ed12c841754705e55c61f9bb893472
Dean Moser <demo9848@colorado.edu>
2017-04-05 11:08:26 -0600

    Timing first pass
    Fire a spell every quarter note behavior

--- Assets/Scripts/HatController.cs
+++ Assets/Scripts/HatController.cs
@@ -5,6 +5,8 @@ using UnityEngine.Audio;
 
 public class HatController : MonoBehaviour {
 
+	public float bpm;
+
 	public float speed;
 	public float jumpForce;
 	public GameObject fireball;
@@ -17,10 +19,18 @@ public class HatController : MonoBehaviour {
 	float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
 
+	private float timePerQ;
+	private int qIter = 1;
+	private int mIter = 1;
+
 	// Use this for initialization
 	void Start () {
+		timePerQ = 60 / bpm;
 		rb = GetComponent<Rigidbody2D>();
+
+		InvokeRepeating ("QueueUp", 0f, timePerQ);
 	}
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("TriggerLand"))
@@ -51,5 +61,16 @@ public class HatController : MonoBehaviour {
 			rb.AddForce (new Vector2(0, jumpForce));
 		}
 		rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+	}
+
+	void QueueUp(){
+		print ("beep"+ qIter);
+		if (qIter % 4 == 1){
+			GameObject newBall = Instantiate(fireball, transform.position, transform.rotation);
+			newBall.GetComponent<Rigidbody2D>().AddForce(new Vector2(500, 200));
+			Destroy (newBall, 1);
+			print ("fire!");
+		}
+		qIter++;
 	}
 }
